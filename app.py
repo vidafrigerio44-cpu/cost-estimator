41cbb11b22bc72b248e7f3aee5bf1fdcb706bd6a
vidafrigerio44-cpu <vidafrigerio44@gmail.com>
2026-02-26 16:19:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="The Next Event | The Next Cost",
-    page_icon="💰",
+     page_icon="assets/favicon-v2.png",
     layout="centered",
 )
 
